c9dbbd885479fd81294aee61004b23f1829ee1cf
Josh Mondy <55757183+mondyjosh@users.noreply.github.com>
2025-02-13 09:42:27 -0600

    Updated Wezterm config.

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,8 +6,14 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
 config.color_scheme = 'Tokyo Night'
+config.font = wezterm.font 'FiraCode Nerd Font Mono'
+
+-- Windows changes
+-- Use Powershell 7 by default instead of CMD
+-- config.default_prog = { 'C:/Program Files/PowerShell/7/pwsh.exe', '-NoLogo' }
 
 -- and finally, return the configuration to wezterm
 return config
+
+-- TODO: Starship terminal
